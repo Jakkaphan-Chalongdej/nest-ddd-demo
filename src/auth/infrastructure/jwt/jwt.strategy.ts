af0d55a15b978b85f209d59604ba8fb340ffe2bf
CuteCat <jakkaphan_5741@hotmail.com>
2025-04-01 13:06:42 +0700

    refactor: remove console log for JWT_SECRET in JwtStrategy

--- src/auth/infrastructure/jwt/jwt.strategy.ts
+++ src/auth/infrastructure/jwt/jwt.strategy.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-console.log('JWT_SECRET', process.env.JWT_SECRET);
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
